9705bc313b79ab4fffbeaadf0c69bca3dc620b71
Md. Mursalin Hasan Nirob <109570432+mursalin49@users.noreply.github.com>
2023-06-30 18:39:51 +0600

    Add files via upload

---
+++ sets.py
@@ -0,0 +1,13 @@
+#Add elethisset = {"apple", "banana", "cherry"}
+thisset = {"apple", "banana", "cherry"}
+mylist = ["kiwi", "orange"]
+
+thisset.update(mylist)
+
+print(thisset)
+#Remove
+thisset = {"apple", "banana", "cherry"}
+
+thisset.remove("banana")
+
+print(thisset)
