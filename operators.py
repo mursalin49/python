d2174156fd952d6b3f5960bab4a63a42b0c99d3d
Md. Mursalin Hasan Nirob <109570432+mursalin49@users.noreply.github.com>
2023-06-30 15:46:56 +0600

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,14 @@
+x = 5
+y = 3
+
+print("sum is:", x + y)
+print(x - y)
+print(x / y)
+print(x * y)
+print(x % y)
+print(x == y) # false
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
