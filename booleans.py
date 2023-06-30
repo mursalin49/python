039f5d29d422a32544c6d333d80cf16c7a33629c
Md. Mursalin Hasan Nirob <109570432+mursalin49@users.noreply.github.com>
2023-06-30 15:47:21 +0600

    Add files via upload

---
+++ booleans.py
@@ -0,0 +1,15 @@
+a = 200
+b = 33
+
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+#Print "YES!" if the function returns True, otherwise print "NO!":
+def myFunction() :
+  return True
+
+if myFunction():
+  print("YES!")
+else:
+  print("NO!")
